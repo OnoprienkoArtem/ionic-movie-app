826efa0f4e33ee5925e80825ebbfa14895640d7b
OnoprienkoArtem <o.artem.e@gmail.com>
2019-11-21 22:36:34 +0200

    refactoring speaker list

--- src/app/pages/speaker-list/speaker-list-routing.module.ts
+++ src/app/pages/speaker-list/speaker-list-routing.module.ts
@@ -3,14 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { SpeakerListPage } from './speaker-list';
 const routes: Routes = [
-  {
-    path: '',
-    component: SpeakerListPage
-  }
+    {
+        path: '',
+        component: SpeakerListPage
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
 })
 export class SpeakerListPageRoutingModule {}
--- src/app/pages/speaker-list/speaker-list.spec.ts
+++ src/app/pages/speaker-list/speaker-list.spec.ts
@@ -10,30 +10,30 @@ import { ConferenceData } from '../../providers/conference-data';
 const confDataSub = {};
 
 describe('SpeakerListPage', () => {
-  let fixture, app;
-  beforeEach(async(() => {
-    const actionSheetSpy = jasmine.createSpyObj('ActionSheetController', [
-      'create'
-    ]);
-    const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
-    const iabSpy = jasmine.createSpyObj('InAppBrowser', ['create']);
+    let fixture, app;
+    beforeEach(async(() => {
+        const actionSheetSpy = jasmine.createSpyObj('ActionSheetController', [
+            'create'
+        ]);
+        const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+        const iabSpy = jasmine.createSpyObj('InAppBrowser', ['create']);
 
-    TestBed.configureTestingModule({
-      declarations: [SpeakerListPage],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA],
-      providers: [
-        { provide: ActionSheetController, useValue: actionSheetSpy },
-        { provide: InAppBrowser, useValue: iabSpy },
-        { provide: Router, useValue: routerSpy },
-        { provide: ConferenceData, useValue: confDataSub }
-      ]
-    }).compileComponents();
-  }));
-  beforeEach(() => {
-    fixture = TestBed.createComponent(SpeakerListPage);
-    app = fixture.debugElement.componentInstance;
-  });
-  it('should create the speaker list page', () => {
-    expect(app).toBeTruthy();
-  });
+        TestBed.configureTestingModule({
+            declarations: [SpeakerListPage],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+            providers: [
+                { provide: ActionSheetController, useValue: actionSheetSpy },
+                { provide: InAppBrowser, useValue: iabSpy },
+                { provide: Router, useValue: routerSpy },
+                { provide: ConferenceData, useValue: confDataSub }
+            ]
+        }).compileComponents();
+    }));
+    beforeEach(() => {
+        fixture = TestBed.createComponent(SpeakerListPage);
+        app = fixture.debugElement.componentInstance;
+    });
+    it('should create the speaker list page', () => {
+        expect(app).toBeTruthy();
+    });
 });
