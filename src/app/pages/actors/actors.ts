b002a9f14b0b86dd28967dea5ffcc4cab8f7171c
OnoprienkoArtem <o.artem.e@gmail.com>
2019-12-09 22:28:52 +0200

    get actors data

--- src/app/pages/actors/actors.ts
+++ src/app/pages/actors/actors.ts
@@ -1,23 +1,35 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { Subscription } from 'rxjs';
+import { MovieService } from '../../providers/movie.service';
 @Component({
   selector: 'actors',
   templateUrl: 'actors.html',
   styleUrls: ['./actors.scss'],
 })
-export class ActorsPage {
-  speakers: any[] = [];
+export class ActorsPage implements OnInit, OnDestroy {
+
+
+
+  private actorsSubscription: Subscription;
 
   constructor(
-    public router: Router
-  ) {}
+    public router: Router,
+    public movieService: MovieService
+  ) { }
 
-  ionViewDidEnter() {
+  ngOnInit() {
+    this.actorsSubscription = this.movieService.getPopularActors()
+      .subscribe(data => {
+        console.log('actors =====>>>', data);
+
+      });
 
   }
 
-
+  ngOnDestroy() {
+    this.actorsSubscription.unsubscribe();
+  }
 
 
 
